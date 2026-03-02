1a8cc3264c64946aeeb258e57df8a3ba1ec86eee
Karanvir Singh Rattan {KV} <karanrattan0420@gmail.com>
2026-03-01 23:24:21 -0700

    Refactor app.js to set up Express server with CORS

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const express = require("express");
 const cors = require("cors");
 
+const postRoutes = require("./routes/post");
+
+const app = express();
+const PORT = 3000;
+
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
+    origin: "http://localhost:5173"
 }));
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api", postRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
